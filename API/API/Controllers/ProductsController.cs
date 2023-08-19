81e5a11a97b4950291e754114c47ce5a782bdc24
Sinan Ergun <ergun.sinan31@gmail.com>
2023-08-19 19:27:15 +0200

    Base du projet

---
+++ API/API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+
+
+    [HttpGet]
+    public List<string> GetProducts()
+    {
+        return new List<string> { "Product 1", "Product 2" };
+    }
+
+    [HttpGet("{id}")]
+    public string GetProduct(int id)
+    {
+        return "This is a product !";
+    }
+}
+
